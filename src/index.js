1deccb88d131af6c6723df985f184f3915adb05c
Sahil Shah <sahilshah2104@gmail.com>
2024-07-17 23:21:01 +0530

    Feat: Added Rate Limiting to the service

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 
 const { ServerConfig } = require('./config');
 const apiRouter = require('./routes');
 const errorHandler = require('./utils/errorHandler');
-const { Auth } = require('./utils/index');
 
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000,
+    max: 3,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
+
+app.use(limiter);
 
 app.use('/api', apiRouter);
 
